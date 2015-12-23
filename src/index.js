b19b7413120b6f6f48a6b8fc931c52d4e1708a0b
JP Richardson <jprichardson@gmail.com>
2015-12-23 10:16:16 -0600

    set node v4 as minimum target. Added travis and appveyor.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-class RipDB {
-  
+export default class RipDB {
+  static create (path, indexFn) {
+    return new RipDB(path, indexFn)
+  }
+
+  constructor (path, indexFn) {
+    this._path = path
+    this._indexFn = indexFn
+  }
+
+  createReader () {
+    
+  }
+
+  createWriter () {
+
+  }
+
+  get path () {
+    return this._path
+  }
 }
